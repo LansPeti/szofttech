939d477ce34b56039e1ea0ccb780977c4f9f834d
Emma Selmeci <mtyselmeci@gmail.com>
2026-04-07 09:48:17 +0200

    Add placeholder register functionality

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const users = require("../data/users");
+const { findByUsername, addUser, DuplicateUserError } = require("../data/users");
 
 router.post("/login", (req, res) => {
     const { username, password } = req.body;
@@ -9,13 +9,34 @@ router.post("/login", (req, res) => {
         return res.status(400).json({ message: "Username and password required!" });
     }
 
-    const user = users.find(u => u.username === username);
+    const user = findByUsername(username);
 
     if (!user || user.password !== password) {
         return res.status(401).json({ message: "Invalid credentials" });
     }
 
     res.status(200).json({ message: "Login successful" });
+})
+
+router.post("/register", (req, res) => {
+    const { username, email, password } = req.body;
+
+    if (!username || !email || !password) {
+        return res.status(400).json({ message: "Username, e-mail and password required!" });
+    }
+
+    try {
+        addUser({username, email, password});
+    } catch (err) {
+        if (err instanceof DuplicateUserError)  {
+            return res.status(409).json({ message: err.message });
+        }
+        
+        console.error("Failed to write users file:", err);
+        return res.status(500).json({ message: "Internal server error" });
+    }
+
+    res.status(200).json({ message: "User registered" });
 })
 
 module.exports = router;
