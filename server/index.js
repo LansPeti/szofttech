cb2c7fce2e72e89e8547aedbfc7167900868eda0
Emma Selmeci <mtyselmeci@gmail.com>
2026-04-11 15:13:58 +0200

    api/login -> api/auth/login

--- server/index.js
+++ server/index.js
@@ -12,7 +12,7 @@ const PORT = 5000;
 app.use(cors());
 app.use(express.json());
 
-app.use("/api", authRoutes);
+app.use("/api/auth", authRoutes);
 
 app.get("/api/hello", (req, res) => {
   res.json({ message: "Hello from Express backend!" });
