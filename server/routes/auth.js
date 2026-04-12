24cd18f2ef926a9b8999768282791432aa664c0f
Emma Selmeci <mtyselmeci@gmail.com>
2026-04-12 13:01:08 +0200

    Feat: finalize login

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -3,28 +3,35 @@ const router = express.Router();
 const { findByUsername, addUser, DuplicateUserError } = require("../data/users");
 const jwt = require("jsonwebtoken");
 const { JWT_SECRET } = require("../config");
+const bcrypt = require("bcrypt");
 
 router.post("/login", (req, res) => {
     const { username, password } = req.body;
 
     if (!username || !password) {
-        return res.status(400).json({ message: "Username and password required!" });
+        return res.status(400).json({ error: "Felhasználónév és jelszó megadása kötelező!" });
     }
 
     const user = findByUsername(username);
 
-    if (!user || user.password !== password) {
-        return res.status(401).json({ message: "Invalid credentials" });
+    if (!user || !bcrypt.compare(password, user.passwordHash)) {
+        return res.status(401).json({ error: "Hibás felhasználónév vagy jelszó" });
     }
 
-    res.status(200).json({ message: "Login successful" });
+    res.status(200).json({
+        id: user.id,
+        username: user.username,
+        email: user.email,
+        avatarColor: user.avatarColor,
+        token: jwt.sign({ id: user.id, username: user.username }, JWT_SECRET, { expiresIn: "24h" })
+    });
 })
 
 router.post("/register", (req, res) => {
     const { username, email, password } = req.body;
 
     if (!username || !email || !password) {
-        return res.status(400).json({ message: "Minden mező kitöltése kötelező" });
+        return res.status(400).json({ error: "Minden mező kitöltése kötelező" });
     }
 
     try {
@@ -37,11 +44,11 @@ router.post("/register", (req, res) => {
         });
     } catch (err) {
         if (err instanceof DuplicateUserError) {
-            return res.status(409).json({ message: err.message });
+            return res.status(409).json({ error: err.message });
         }
 
         console.error("Failed to write users file:", err);
-        return res.status(500).json({ message: "Belső kiszolgálóhiba" });
+        return res.status(500).json({ error: "Belső kiszolgálóhiba" });
     }
 })
 
